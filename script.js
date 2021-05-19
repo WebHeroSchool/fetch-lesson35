49d8d0f51f7e7b0740777682a5edd0cb91587698
Nastenka <voronkova_ab@mail.ru>
2021-05-19 12:35:56 +0500

    Исправила ошибки(получение ника из ссылки и сделала имя пользователя ссылкоц на его гитхаб)

---
+++ script.js
@@ -0,0 +1,37 @@
+const body = document.body;
+let url = window.location.toString();
+
+function getUserName(url) {
+  let allUrl = url.split('=');
+  let name = allUrl[1];
+  if (!name) {
+    name = 'defunkt';
+  }
+
+  return 'https://api.github.com/users/' + name;
+}
+
+let urlFetch = getUserName(url);
+
+fetch(urlFetch)
+  .then(res => res.json())
+  .then(json => {
+    if (json.message !== 'Not Found') {
+      let img = document.createElement('img');
+      img.src = json.avatar_url;
+      body.append(img);
+
+      let name = document.createElement('a');
+      name.textContent = json.name;
+      name.href = json.html_url;
+      body.prepend(name);
+
+      let bio = document.createElement('div');
+      bio.textContent = json.bio;
+      body.append(bio);
+    } else {
+      let error = document.createElement('p');
+      error.textContent = 'Информация о пользователе не доступна';
+      body.append(error);
+    }
+  })
