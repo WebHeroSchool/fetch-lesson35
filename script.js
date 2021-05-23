1e57848d33c072148462c151e8b66ddeca4e7205
Nastenka <voronkova_ab@mail.ru>
2021-05-23 20:22:30 +0500

    Выполнила задание по промисам

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const body = document.body;
-let url = window.location.toString();
-let urlFetch = getUserName(url);
+let nowUrl = window.location.toString();
+let url = getUserName(nowUrl);
+let nowDate = new Date();
 
 function getUserName(url) {
   let allUrl = url.split('=');
@@ -12,7 +13,21 @@ function getUserName(url) {
   return 'https://api.github.com/users/' + name;
 }
 
-fetch(urlFetch)
+const getDate = new Promise((resolve, reject) => {
+  setTimeout(() => nowDate ? resolve(nowDate) : reject('Проблемы с получением даты'), 2000);
+});
+const getUrl = new Promise((resolve,reject) => {
+  setTimeout(() => url ? resolve(url) : reject('Проблемы с получением url'), 3000);
+});
+
+Promise.all([getDate, getUrl])
+  .then(([date, url]) => {
+    let htmlDate = document.createElement('p');
+    htmlDate.textContent = date;
+    body.append(htmlDate);
+    
+    return fetch(url);
+  })
   .then(res => res.json())
   .then(json => {
     if (json.message !== 'Not Found') {
@@ -28,9 +43,13 @@ fetch(urlFetch)
       let bio = document.createElement('div');
       bio.textContent = json.bio;
       body.append(bio);
+
+      let preloader = document.getElementById('preloader');
+      json.onload = preloader.classList.add('hidden');
     } else {
       let error = document.createElement('p');
       error.textContent = 'Информация о пользователе не доступна';
       body.append(error);
     }
   })
+  .catch(err => console.log(err));
